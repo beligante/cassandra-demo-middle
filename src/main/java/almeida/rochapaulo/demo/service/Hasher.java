8896968ac986191cc433600e7361d0418b108bd5
Paulo Almeida <palmeida@kenzanmedia.com>
2016-10-20 11:32:42 -0200

    typo fix

--- src/main/java/almeida/rochapaulo/demo/service/Hasher.java
+++ src/main/java/almeida/rochapaulo/demo/service/Hasher.java
@@ -14,23 +14,22 @@ public class Hasher {
 		}
 		return instance;
 	}
-	
+
 	private Hasher() {
 		super();
 	}
-	
-	public String hash(String source) {
-	
-		try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(source.getBytes("UTF-8"));
-            byte[] digest = md.digest();
 
-            return String.format("%064x", new java.math.BigInteger(1, digest));
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-        	throw new RuntimeException(e); 
-        }
-		
+	public String hash(String source) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(source.getBytes("UTF-8"));
+			byte[] digest = md.digest();
+
+			return String.format("%064x", new java.math.BigInteger(1, digest));
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
-	
+
 }
